e2f99081429e292590d945b8569828af4066c300
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-11 23:09:51 +0100

    added some correct code

--- script.js
+++ script.js
@@ -3,43 +3,43 @@ document.addEventListener('DOMContentLoaded', () => {
     //card options
     const cardArray = [
         {
-            names: 'camille',
+            name: 'camille',
             img: 'images/img1.png'
         },
         {
-            names: 'camille',
+            name: 'camille',
             img: 'images/img1.png'
         },
         {
-            names: 'eileen',
+            name: 'eileen',
             img: 'images/img2.png'
         },  
         {
-            names: 'eileen',
+            name: 'eileen',
             img: 'images/img2.png'
         },
         {
-            names: 'brandi',
+            name: 'brandi',
             img: 'images/img3.png'
         },
         {
-            names: 'brandi',
+            name: 'brandi',
             img: 'images/img3.png'
         },
         {
-            names: 'adrienne',
+            name: 'adrienne',
             img: 'images/img4.png'
         },
         {
-            names: 'adrienne',
+            name: 'adrienne',
             img: 'images/img4.png'
         },
         {
-            names: 'taylor',
+            name: 'taylor',
             img: 'images/img5.png'
         },
         {
-            names: 'taylor',
+            name: 'taylor',
             img: 'images/img5.png'
         },
         {
@@ -47,74 +47,120 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/img6.png'
         },
         {
-            names: 'erika',
+            name: 'erika',
             img: 'images/img6.png'
         },
         {
-            names: 'dorit',
+            name: 'dorit',
             img: 'images/img7.png'
         },
         {
-            names: 'dorit',
+            name: 'dorit',
             img: 'images/img7.png'
         },
         {
-            names: 'lisar',
+            name: 'lisar',
             img: 'images/img8.png'
         },
         {
-            names: 'lisar',
+            name: 'lisar',
             img: 'images/img8.png'
         },
         {
-            names: 'lisav',
+            name: 'lisav',
             img: 'images/img9.png'
         },
         {
-            names: 'lisav',
+            name: 'lisav',
             img: 'images/img9.png'
         },
         {
-            names: 'kyle',
+            name: 'kyle',
             img: 'images/img10.png'
         },
         {
-            names: 'kyle',
+            name: 'kyle',
             img: 'images/img10.png'
         },
+        {
+            name: 'yolanda',
+            img: 'images/img11.png'
+        },
+        {
+            name: 'yolanda',
+            img: 'images/img11.png'
+        },
+        {
+            name: 'garcelle',
+            img: 'images/img12.png'
+        },
+        {
+            name: 'garcelle',
+            img: 'images/img12.png'
+        }
     ]
     cardArray.sort(() => 0.5 - Math.random())
 
     const grid = document.querySelector('.grid');
-    const resultDisplay = document.querySelector('#result');
-    var cardsChosen = [];
-    var cardsChosenId = [];
-    const cardsWon = [];
+    const resultDisplay = document.querySelector('#result')
+    var cardsChosen = []
+    var cardsChosenId = []
+    const cardsWon = []
 
-    //create your board
+    // create your board
     function createBoard() {
-        for (let i = 0; i < cardArray.length; i++) {
-            var card = document.createElement('img');
-            card.setAttribute('src', 'images/blank.png');
-            card.setAttribute('data-id', i);
-            card.addEventListener('click', flipCard);
-            grid.appendChild(card);
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img')
+        card.setAttribute('src', 'images/cover.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        grid.appendChild(card);
+
         }
     }
 
+    //check for matches
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+    
+        if (optionOneId == optionTwoId) {
+            cards[optionOneId].setAttribute('src', 'images/cover.png')
+            cards[optionTwoId].setAttribute('src', 'images/cover.png')
+            alert('You have clicked the same image!')
+        }
+        else if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match!')
+            cards[optionOneId].setAttribute('src', 'images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'images/blank.png')
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListener('click', flipCard)
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'images/cover.png')
+            cards[optionTwoId].setAttribute('src', 'images/cover.png')
+            alert('Sorry, try again!')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = ' ' + 'Congratulations! You found them all!'
+        }
+    }
 
+    //flip your card
+    function flipCard() {
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length ===2) {
+            setTimeout(checkForMatch, 500);
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+createBoard()
 
 })
