5ebdd3f44966efd2d5fc40d347701b698043fd1a
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-11 22:06:15 +0100

    added the cardArray

---
+++ script.js
@@ -0,0 +1,107 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    //card options
+    const cardArray = [
+        {
+            names: 'camille',
+            img: 'images/img1.png'
+        },
+        {
+            names: 'camille',
+            img: 'images/img1.png'
+        },
+        {
+            names: 'eileen',
+            img: 'images/img2.png'
+        },  
+        {
+            names: 'eileen',
+            img: 'images/img2.png'
+        },
+        {
+            names: 'brandi',
+            img: 'images/img3.png'
+        },
+        {
+            names: 'brandi',
+            img: 'images/img3.png'
+        },
+        {
+            names: 'adrienne',
+            img: 'images/img4.png'
+        },
+        {
+            names: 'adrienne',
+            img: 'images/img4.png'
+        },
+        {
+            names: 'taylor',
+            img: 'images/img5.png'
+        },
+        {
+            names: 'taylor',
+            img: 'images/img5.png'
+        },
+        {
+            names: 'erika',
+            img: 'images/img6.png'
+        },
+        {
+            names: 'erika',
+            img: 'images/img6.png'
+        },
+        {
+            names: 'dorit',
+            img: 'images/img7.png'
+        },
+        {
+            names: 'dorit',
+            img: 'images/img7.png'
+        },
+        {
+            names: 'lisar',
+            img: 'images/img8.png'
+        },
+        {
+            names: 'lisar',
+            img: 'images/img8.png'
+        },
+        {
+            names: 'lisav',
+            img: 'images/img9.png'
+        },
+        {
+            names: 'lisav',
+            img: 'images/img9.png'
+        },
+        {
+            names: 'kyle',
+            img: 'images/img10.png'
+        },
+        {
+            names: 'kyle',
+            img: 'images/img10.png'
+        },
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
